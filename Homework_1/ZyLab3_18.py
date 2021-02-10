a4c66b7a6c6f40ff32855ee106e5d965f7c92f3e
jeremiahsoyebo <77858168+jeremiahsoyebo@users.noreply.github.com>
2021-02-09 22:35:01 -0600

    Add files via upload
    3.18 final submission (not sure how to delete files

--- Homework_1/ZyLab3_18.py
+++ Homework_1/ZyLab3_18.py
@@ -12,10 +12,7 @@ print('Cans needed:', cansNeeded, 'can(s)\n')
 
 print('Choose a color to paint the wall:')
 wallColor = input()
-colorPrices = {'Red': 35, 'Blue': 25, 'Green': 23}
-paintCost = cansNeeded * (colorPrices['Red'])
-# i dont know if i should email or if a comment can be left on the assignment
-# how do i enter multiple values from a dictionary?
-# i kept getting errors and i couldn't find it anywhere, so i just used red to match one zyBooks output
+colorPrices = {'Red': 35, 'Blue': 25, 'Green': 23, 'red': 35, 'blue': 25, 'green': 23}
+paintCost = cansNeeded * (colorPrices[wallColor])
 
 print('Cost of purchasing {} paint: ${}'.format(wallColor, paintCost))
